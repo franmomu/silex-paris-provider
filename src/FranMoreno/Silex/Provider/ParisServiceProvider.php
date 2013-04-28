b592230165678e01b9d2cdd9da93523795573b67
Fran Moreno <franmomu@gmail.com>
2013-04-28 17:32:41 +0200

    Added paris.db service

--- src/FranMoreno/Silex/Provider/ParisServiceProvider.php
+++ src/FranMoreno/Silex/Provider/ParisServiceProvider.php
@@ -10,7 +10,14 @@ class ParisServiceProvider implements ServiceProviderInterface
 {
     public function register(Application $app)
     {
-        $app['paris'] = $app->share(function ($app) {
+        $app['paris.initializer'] = $app->protect(function () use ($app) {
+            static $initialized = false;
+
+            if ($initialized) {
+                return;
+            }
+
+            $initialized = true;
 
             $config = isset($app['idiorm.config']) ? $app['idiorm.config'] : null;
             \ORM::configure($config);
@@ -18,8 +25,18 @@ class ParisServiceProvider implements ServiceProviderInterface
             if (isset($app['paris.model.prefix'])) {
                 \Model::$auto_prefix_models = $app['paris.model.prefix'];
             }
+        });
+
+        $app['paris'] = $app->share(function ($app) {
+            $app['paris.initializer']();
 
             return new ParisService();
+        });
+
+        $app['paris.db'] = $app->share(function ($app) {
+            $app['paris.initializer']();
+
+            return \ORM::get_db();
         });
     }
 
